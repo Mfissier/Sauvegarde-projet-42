c6f4a00d169fe15a7fc2b8f030f14126a850fc01
Maxime FISSIER <mfissier@e1r12p23.42.fr>
2018-07-09 10:16:15 +0200

    Day2 sauvegard perso

--- Save_perso/Day02/ex01/ft_print_alphabet.c
+++ Save_perso/Day02/ex01/ft_print_alphabet.c
@@ -5,31 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfissier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/08 08:25:49 by mfissier          #+#    #+#             */
-/*   Updated: 2018/07/09 09:15:31 by mfissier         ###   ########.fr       */
+/*   Created: 2018/07/09 09:29:16 by mfissier          #+#    #+#             */
+/*   Updated: 2018/07/09 10:00:34 by mfissier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
-int ft_putchar(char c)
+int ft_putchar (char c)
 {
 	write(1, &c, 1);
 	return (0);
 }
 
-void ft_print_alphabet(void)
+void ft_print_reverse_alphabet(void)
 {
-	char alphabet = 'a';
-	char end_alphabet = 'z';
-	while (alphabet <= end_alphabet)
+	char alphabet_begin_with_z = 'z';
+	char alphabet_end_with_a = 'a';
+	while (alphabet_begin_with_z >= alphabet_end_with_a)
 	{
-		ft_putchar(alphabet);
-		alphabet++;	
+		ft_putchar(alphabet_begin_with_z);
+		alphabet_begin_with_z --; 
 	}
 }
 
-int main(void)
+int main (void)
 {
-ft_print_alphabet();
+	ft_print_reverse_alphabet();
 }
