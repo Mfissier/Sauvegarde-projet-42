8f3bab98ad99963fedbf289b8c0349054aec5fad
Maxime FISSIER <mfissier@e1r4p13.42.fr>
2018-07-23 04:14:37 +0200

    Add de fin dejourne

---
+++ No_git_day/J06_NP/ex02/ft_print_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfissier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/22 16:15:59 by mfissier          #+#    #+#             */
+/*   Updated: 2018/07/23 04:11:29 by mfissier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (argv[i] != '\0' && argc > 1)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+}
